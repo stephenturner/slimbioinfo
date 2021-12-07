54142988038bd27e92dce729c76d6a371a1c7114
Stephen Turner <vustephen@gmail.com>
2021-12-07 19:31:50 +0000

    add gatk and commented dragmap to build

--- BUILD.sh
+++ BUILD.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-docker build --no-cache -t stephenturner/bcftools bcftools/
-docker build --no-cache -t stephenturner/minimap2 minimap2/
 docker build --no-cache -t stephenturner/akt akt/
-docker build --no-cache -t stephenturner/bwa bwa/
-docker build --no-cache -t stephenturner/ped-sim ped-sim/
+docker build --no-cache -t stephenturner/bcftools bcftools/
 docker build --no-cache -t stephenturner/bedtools bedtools/
+docker build --no-cache -t stephenturner/bwa bwa/
+# docker build --no-cache -t stephenturner/dragmap dragmap/
+docker build --no-cache -t stephenturner/gatk gatk/
+docker build --no-cache -t stephenturner/minimap2 minimap2/
+docker build --no-cache -t stephenturner/ped-sim ped-sim/
