fba9a7c47ac8c78f1112cfcb23551213595f99ed
Stephen Turner <vustephen@gmail.com>
2022-01-07 10:26:09 +0000

    entrypoint everything except htslib

--- BUILD.sh
+++ BUILD.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
 docker build --no-cache -t stephenturner/akt akt/
 docker build --no-cache -t stephenturner/bcftools bcftools/
 docker build --no-cache -t stephenturner/bedtools bedtools/
 docker build --no-cache -t stephenturner/bwa bwa/
-# docker build --no-cache -t stephenturner/dragmap dragmap/
+docker build --no-cache -t stephenturner/core core/
+docker build --no-cache -t stephenturner/dragmap dragmap/
 docker build --no-cache -t stephenturner/gatk gatk/
+docker build --no-cache -t stephenturner/htslib htslib/
+docker build --no-cache -t stephenturner/kraken2 kraken2/
 docker build --no-cache -t stephenturner/minimap2 minimap2/
 docker build --no-cache -t stephenturner/ped-sim ped-sim/
+docker build --no-cache -t stephenturner/rasusa rasusa/
+docker build --no-cache -t stephenturner/seqkit seqkit/
+docker build --no-cache -t stephenturner/snpeff snpeff/
+docker build --no-cache -t stephenturner/vt vt/
